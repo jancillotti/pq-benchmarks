805df9159fe3153514378f445116aa00c77f9986
Geoff Twardokus <gdt5762@rit.edu>
2023-09-29 21:10:53 -0400

    Add sphincs files

--- Makefile
+++ Makefile
@@ -3,20 +3,20 @@ all: arm x86
 x86:
 	g++ -o xmss-x86 xmss.cpp util.cpp -I$(HOME)/botan-intel/include/botan-3/ -I$(HOME)/botan-intel/include/botan-3/botan $(HOME)/botan-x86/libbotan-3.a --std=c++20 -static
 	g++ -o ecdsa-x86 ecdsa.cpp util.cpp -I$(HOME)/botan-intel/include/botan-3/ -I$(HOME)/user/botan-intel/include/botan-3/botan/ $(HOME)/botan-x86/libbotan-3.a --std=c++20 -static
+	g++ -o dilithium-x86 dilithium.cpp util.cpp -I$(HOME)/botan-intel/include/botan-3/ -I$(HOME)/user/botan-intel/include/botan-3/botan/ $(HOME)/botan-x86/libbotan-3.a --std=c++20 -static
 	g++ -o falcon-x86 falcon.cpp util.cpp -static -I$(HOME)/liboqs-x86/include $(HOME)/liboqs-x86/lib/liboqs.a
-
+	g++ -o sphincs-x86 sphincs.cpp util.cpp -static -I$(HOME)/liboqs-x86/include $(HOME)/liboqs-x86/lib/liboqs.a
 
 arm:
 	aarch64-linux-gnu-g++ -o xmss-arm xmss.cpp util.cpp -I$(HOME)/botan-aarch64/include/botan-3/ $(HOME)/botan-arm/libbotan-3.a --std=c++20 -static
 	aarch64-linux-gnu-g++ -o ecdsa-arm ecdsa.cpp util.cpp -I$(HOME)/botan-aarch64/include/botan-3/ -I$(HOME)botan-aarch64/include/botan-3/botan $(HOME)/botan-arm/libbotan-3.a --std=c++20 -static
+	aarch64-linux-gnu-g++ -o dilithium-arm dilithium.cpp util.cpp -I$(HOME)/botan-aarch64/include/botan-3/ -I$(HOME)botan-aarch64/include/botan-3/botan $(HOME)/botan-arm/libbotan-3.a --std=c++20 -static
 	aarch64-linux-gnu-g++ -o falcon-arm falcon.cpp util.cpp -static -I$(HOME)/liboqs-aarch64/include $(HOME)/liboqs-aarch64/lib/liboqs.a
-
-#	aarch64-linux-gnu-g++ timing.cpp -o timing-arm -static -I$(HOME)/liboqs-aarch64/include $(HOME)/liboqs-aarch64/lib/liboqs.a
-
+	aarch64-linux-gnu-g++ -o sphincs-arm sphincs.cpp util.cpp -static -I$(HOME)/liboqs-aarch64/include $(HOME)/liboqs-aarch64/lib/liboqs.a
 clean:
-	@rm xmss-arm
-	@rm xmss-x86
-	@rm ecdsa-arm
-	@rm ecdsa-x86
-	@rm falcon-arm
-	@rm falcon-x86
+	@$(RM) xmss-arm
+	@$(RM)  xmss-x86
+	@$(RM)  ecdsa-arm
+	@$(RM)  ecdsa-x86
+	@$(RM) falcon-arm
+	@$(RM) falcon-x86
---
+++ sphincs.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <oqs/oqs.h>
+#include <chrono>
+#include <vector>
+
+#include "util.h"
+
+#define MESSAGE_LEN 2500
+
+int sign_and_verify() {
+
+	OQS_STATUS rc;
+
+        uint8_t public_key[OQS_SIG_sphincs_sha2_128s_simple_length_public_key];
+        uint8_t secret_key[OQS_SIG_sphincs_sha2_128s_simple_length_secret_key];
+        uint8_t message[MESSAGE_LEN];
+        uint8_t signature[OQS_SIG_sphincs_sha2_128s_simple_length_signature];
+        size_t message_len = MESSAGE_LEN;
+        size_t signature_len;
+
+        OQS_randombytes(message, message_len);
+
+        rc = OQS_SIG_sphincs_sha2_128s_simple_keypair(public_key, secret_key);
+        if (rc != OQS_SUCCESS) {
+       		exit(EXIT_FAILURE);
+       	}
+	auto start = std::chrono::high_resolution_clock::now();
+
+	rc = OQS_SIG_sphincs_sha2_128s_simple_sign(signature, &signature_len, message, message_len, secret_key);
+	
+        auto stop = std::chrono::high_resolution_clock::now();
+	sign_times.push_back(std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count());
+
+	start = std::chrono::high_resolution_clock::now();
+
+	rc = OQS_SIG_sphincs_sha2_128s_simple_verify(message, message_len, signature, signature_len, public_key);
+
+	if (rc == OQS_SUCCESS) {
+		std::cout << "Success." << std::endl;
+		stop = std::chrono::high_resolution_clock::now();
+		verify_times.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count());
+		return 0;
+	}
+	else {
+		std::cout << "Error." << std::endl;
+		return 1;
+	}
+	
+}
+
+int main() {
+        unsigned long long average_sign_time, average_verify_time, stddev_sign_time, stddev_verify_time;
+
+	for(int i = 0; i < ITERATIONS; i++) {
+		std::cout << "i=" << i << "\t";
+		sign_and_verify();
+	}
+
+	get_average_sign_time(average_sign_time, sign_times);
+	get_average_verify_time(average_verify_time, verify_times);
+	get_stddev_sign(stddev_sign_time, sign_times);
+	get_stddev_verify(stddev_verify_time, verify_times);
+
+	record_results(average_sign_time,
+		   average_verify_time,
+		   stddev_sign_time,
+		   stddev_verify_time,
+		   "sphincs_results.txt");
+
+	sign_times.clear();
+	verify_times.clear();
+
+	return 0;
+}
