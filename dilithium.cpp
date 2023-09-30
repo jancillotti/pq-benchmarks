ef02ce5681f972a0d488460bea9e8ad340351406
Geoff Twardokus <geoff.twardokus@outlook.com>
2023-09-29 20:56:00 -0400

    Add files via upload

---
+++ dilithium.cpp
@@ -0,0 +1,70 @@
+//
+// Created by geoff on 7/31/23.
+//
+#include <botan/auto_rng.h>
+#include <botan/dilithium.h>
+#include <botan/pubkey.h>
+
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+#include "util.h"
+
+#define ITERATIONS 1000
+
+int sign_and_verify() {
+
+    Botan::AutoSeeded_RNG rng;
+    // Generate Dilithium keypair
+    auto priv_key = Botan::Dilithium_PrivateKey(rng, Botan::DilithiumMode(Botan::DilithiumMode::Dilithium4x4));
+    const auto pub_key = priv_key.public_key();
+
+//    const std::string message("This is a tasty burger!");
+//    const std::vector<uint8_t> msgvec(message.data(), message.data() + message.size());
+
+    // sign data
+    auto signer = Botan::PK_Signer(priv_key, rng, "Randomized");
+
+    Botan::secure_vector<uint8_t> msg{0x01, 0x02, 0x03, 0x04};
+
+    auto start = std::chrono::high_resolution_clock::now();
+    signer.update(msg.data(), msg.size());
+    std::vector<uint8_t> sig = signer.signature(rng);
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    sign_times.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count());
+
+    //******************************VERIFY**********************************
+    Botan::PK_Verifier verifier(*pub_key, "");
+
+    start = std::chrono::high_resolution_clock::now();
+    verifier.update(msg);
+    stop = std::chrono::high_resolution_clock::now();
+    std::cout << "is " << (verifier.check_signature(sig) ? "valid" : "invalid") << std::endl;
+    verify_times.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count());
+    return 0;
+}
+
+int main() {
+    unsigned long long average_sign_time, average_verify_time, stddev_sign_time, stddev_verify_time;
+    for(int i = 0; i < ITERATIONS; i++) {
+        std::cout << "i=" << i << "\t";
+        sign_and_verify();
+    }
+    get_average_sign_time(average_sign_time, sign_times);
+    get_average_verify_time(average_verify_time, verify_times);
+    get_stddev_sign(stddev_sign_time, sign_times);
+    get_stddev_verify(stddev_verify_time, verify_times);
+
+    record_results(average_sign_time,
+                   average_verify_time,
+                   stddev_sign_time,
+                   stddev_verify_time,
+                   "dilithium_results.txt");
+
+    sign_times.clear();
+    verify_times.clear();
+
+    return 0;
+}
